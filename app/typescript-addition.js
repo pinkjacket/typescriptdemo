5604c9c1cb966b24d36ea271f04d698fa29001b3
James Donlan <jacketlizard@live.com>
2018-02-19 14:07:42 -0800

    typescript lesson part 2, optional typing

---
+++ app/typescript-addition.js
@@ -0,0 +1,4 @@
+var firstNumber = parseInt(prompt('Please enter a number.'));
+var secondNumber = parseInt(prompt('Enter another number.'));
+var sum = firstNumber + secondNumber;
+alert("The sum of your two numbers is: " + sum);
